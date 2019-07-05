b1629d751e6a95428f9ff7901e02b4c57d048fde
Alex Kiura <kiuraalex@gmail.com>
2019-07-05 08:30:24 +0300

    Move weather fetching logic to service

--- app/services/WeatherSevice.scala
+++ app/services/WeatherSevice.scala
@@ -1,11 +1,14 @@
+package services
+
 import play.api.libs.ws.WSClient
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-class WeatherSevice(wsClient: WSClient){
+class WeatherService(wsClient: WSClient){
     def getTemperature(lat: Double, lon: Double): Future[Double] = {
-        val weatherResponseF = ws.url("https://samples.openweathermap.org/data/2.5/" +
+        val weatherResponseF = wsClient.url("https://samples.openweathermap.org/data/2.5/" +
             "weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22").get()
         weatherResponseF.map { weatherResponse =>
         val weatherJson = weatherResponse.json
