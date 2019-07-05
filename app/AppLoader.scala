59f00e9e606306ec190cad3d3c7bd649003be409
Alex Kiura <kiuraalex@gmail.com>
2019-07-05 11:03:28 +0300

    Add onstart and onstop hooks

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -11,6 +11,8 @@ import _root_.controllers.AssetsComponents
 import play.filters.HttpFiltersComponents
 import services.{SunService, WeatherService}
 
+import scala.concurrent.Future
+
 
 class AppApplicationLoader extends ApplicationLoader {
     def load(context: Context) = {
@@ -24,10 +26,20 @@ class AppApplicationLoader extends ApplicationLoader {
 class AppComponents(context: Context) extends
     BuiltInComponentsFromContext(context) with AhcWSComponents
     with AssetsComponents with HttpFiltersComponents {
+        private val log = Logger(this.getClass)
         override lazy val controllerComponents = wire[DefaultControllerComponents]
         lazy val prefix: String = "/"
         lazy val router: Router = wire[Routes]
         lazy val sunService = wire[SunService]
         lazy val weatherService = wire[WeatherService]
         lazy val applicationController = wire[Application]
+
+        applicationLifecycle.addStopHook { () =>
+            log.info("The app is about to stop")
+            Future.successful(Unit)
+        }
+
+        val onStart = {
+            log.info("The app is starting....")
+        }
     } 
